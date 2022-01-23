48463bdf2d623d8811cca1eb8f1d89f8234dafb9
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2022-01-23 16:26:19 +0100

    1.16.5-1.2 update

---
+++ main/java/fr/samlegamer/potionring/PotionRing.java
@@ -0,0 +1,36 @@
+package fr.samlegamer.potionring;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import fr.samlegamer.potionring.items.PRItemsRegistry;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(value = PotionRing.MODID)
+@Mod.EventBusSubscriber(modid = PotionRing.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class PotionRing
+{
+	public static final String MODID = "potionring";
+	
+	public static final Logger log = LogManager.getLogger();
+	
+	public PotionRing()
+	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::ClientSetup);
+				
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		PRItemsRegistry.ITEMS_REGISTRY.register(bus);
+	}
+	
+	private void setup(FMLCommonSetupEvent event)
+	{
+		log.info("Potion of Ring Charged");
+	}
+	
+	private void ClientSetup(FMLClientSetupEvent event) {}
+}
---
+++ main/java/fr/samlegamer/potionring/items/RingOfHasteItem.java
@@ -0,0 +1,52 @@
+package fr.samlegamer.potionring.items;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+public class RingOfHasteItem extends Item implements ICurioItem
+{
+	public RingOfHasteItem(Properties p_i48487_1_)
+	{
+		super(p_i48487_1_);
+	}
+	
+	public boolean isFoil(ItemStack p_77636_1_)
+	{
+	      return true;
+	}
+	
+	@Override
+	public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack)
+	{
+		if(!livingEntity.hasEffect(Effects.DIG_SPEED))
+		{
+            EffectInstance effectInstance = new EffectInstance(Effects.DIG_SPEED, Integer.MAX_VALUE, 0, true, false);
+            if(livingEntity.level.isClientSide) effectInstance.getDuration();
+            livingEntity.addEffect(effectInstance);
+		}
+		else if(CuriosApi.getCuriosHelper().findCurios(livingEntity, this).size() >= 2)
+		{
+            EffectInstance effectInstance = new EffectInstance(Effects.DIG_SPEED, Integer.MAX_VALUE, 1, true, false);
+            if(livingEntity.level.isClientSide) effectInstance.getDuration();
+            livingEntity.addEffect(effectInstance);
+		}
+	}
+    	
+    	@Override
+    	public boolean isValidRepairItem(ItemStack p_82789_1_, ItemStack p_82789_2_)
+    	{
+    		return p_82789_1_.getItem() == Items.GOLD_INGOT;
+    	}
+    	
+    @Override
+    public boolean canUnequip(String identifier, LivingEntity livingEntity, ItemStack stack)
+    {
+    	return livingEntity.removeEffect(Effects.DIG_SPEED);
+    }
+}
