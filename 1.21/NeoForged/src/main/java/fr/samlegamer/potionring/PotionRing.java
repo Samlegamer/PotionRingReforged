fdcfe3b5bd9764c82ab498c541a297848815e0ed
Samlegamer <samlegamer.yt@gmail.com>
2024-07-15 20:04:03 +0200

    port to 1.21 Fabric/NeoForge

---
+++ 1.21/NeoForged/src/main/java/fr/samlegamer/potionring/PotionRing.java
@@ -0,0 +1,55 @@
+package fr.samlegamer.potionring;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import fr.samlegamer.potionring.item.PRItemsRegistry;
+
+@Mod(value = PotionRing.MODID)
+public class PotionRing
+{
+	public static final String MODID = "potionring";
+	public static final Logger log = LogManager.getLogger();
+	
+	public static final MobEffect growing = BuiltInRegistries.MOB_EFFECT.get(ResourceLocation.fromNamespaceAndPath("sizeshiftingpotions", "growing"));
+	public static final MobEffect shrinking = BuiltInRegistries.MOB_EFFECT.get(ResourceLocation.fromNamespaceAndPath("sizeshiftingpotions", "shrinking"));
+	public static final MobEffect widening = BuiltInRegistries.MOB_EFFECT.get(ResourceLocation.fromNamespaceAndPath("sizeshiftingpotions", "widening"));
+	public static final MobEffect thinning = BuiltInRegistries.MOB_EFFECT.get(ResourceLocation.fromNamespaceAndPath("sizeshiftingpotions", "thinning"));
+	
+	public PotionRing(IEventBus modEventBus)
+	{
+		log.info("Potion Ring - REFORGED Loading...");
+		modEventBus.addListener(this::addToTab);
+		PRItemsRegistry.addons();
+		PRItemsRegistry.ITEMS_REGISTRY.register(modEventBus);
+		log.info("Potion Ring - REFORGED is Charged !");
+	}
+	
+	private void addToTab(BuildCreativeModeTabContentsEvent event)
+	{
+	    if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS)
+	    {
+	    	event.accept(PRItemsRegistry.POTION_RING);
+	    	event.accept(PRItemsRegistry.RING_OF_REGENERATION);
+	    	event.accept(PRItemsRegistry.RING_OF_HASTE);
+	    	event.accept(PRItemsRegistry.RING_OF_JUMP_BOOST);
+	    	event.accept(PRItemsRegistry.RING_OF_RESISTANCE);
+	    	event.accept(PRItemsRegistry.RING_OF_SPEED);
+	    	event.accept(PRItemsRegistry.RING_OF_STRENGH);
+	    	if(ModList.get().isLoaded("sizeshiftingpotions"))
+			{
+		    	event.accept(PRItemsRegistry.ring_of_growing);
+		    	event.accept(PRItemsRegistry.ring_of_shrinking);
+		    	event.accept(PRItemsRegistry.ring_of_widening);
+		    	event.accept(PRItemsRegistry.ring_of_thinning);
+			}
+	    }
+	}
+}
---
+++ 1.21/NeoForged/src/main/java/fr/samlegamer/potionring/item/PRItemsRegistry.java
@@ -0,0 +1,43 @@
+package fr.samlegamer.potionring.item;
+
+import fr.samlegamer.potionring.PotionRing;
+import net.minecraft.world.item.Item;
+import net.neoforged.fml.ModList;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class PRItemsRegistry
+{
+	public static final DeferredRegister.Items ITEMS_REGISTRY = DeferredRegister.createItems(PotionRing.MODID);
+
+	public static final Item.Properties RING = new Item.Properties().stacksTo(1);
+	
+	public static final DeferredItem<Item> POTION_RING = ITEMS_REGISTRY.register("potion_ring", () -> new PotionRingItem(RING, PRTypes.none));
+	public static final DeferredItem<Item> RING_OF_REGENERATION = ITEMS_REGISTRY.register("ring_of_regeneration", () -> new PotionRingItem(RING, PRTypes.regeneration));
+	public static final DeferredItem<Item> RING_OF_HASTE = ITEMS_REGISTRY.register("ring_of_haste", () -> new PotionRingItem(RING, PRTypes.haste));
+	public static final DeferredItem<Item> RING_OF_JUMP_BOOST = ITEMS_REGISTRY.register("ring_of_jump_boost", () -> new PotionRingItem(RING, PRTypes.jump));
+	public static final DeferredItem<Item> RING_OF_RESISTANCE = ITEMS_REGISTRY.register("ring_of_resistance", () -> new PotionRingItem(RING, PRTypes.resistance));
+	public static final DeferredItem<Item> RING_OF_SPEED = ITEMS_REGISTRY.register("ring_of_speed", () -> new PotionRingItem(RING, PRTypes.speed));
+	public static final DeferredItem<Item> RING_OF_STRENGH = ITEMS_REGISTRY.register("ring_of_strengh", () -> new PotionRingItem(RING, PRTypes.strength));
+
+	public static DeferredItem<Item> ring_of_growing, ring_of_shrinking, ring_of_widening, ring_of_thinning;
+
+	public static void addons()
+	{
+		if(ModList.get().isLoaded("sizeshiftingpotions"))
+		{
+			PotionRing.log.info("Potion Ring - REFORGED : Charged SizeShiftingPotions Compat");
+			ring_of_growing = ITEMS_REGISTRY.register("ring_of_growing", () -> new PotionRingItem(RING, PRTypes.growing));
+			ring_of_shrinking = ITEMS_REGISTRY.register("ring_of_shrinking", () -> new PotionRingItem(RING, PRTypes.shrinking));
+			ring_of_widening = ITEMS_REGISTRY.register("ring_of_widening", () -> new PotionRingItem(RING, PRTypes.widening));
+			ring_of_thinning = ITEMS_REGISTRY.register("ring_of_thinning", () -> new PotionRingItem(RING, PRTypes.thinning));
+		}
+		else
+		{
+			ring_of_growing = ITEMS_REGISTRY.register("ring_of_growing", () -> new PotionRingItem(new Item.Properties().stacksTo(1), PRTypes.none));
+			ring_of_shrinking = ITEMS_REGISTRY.register("ring_of_shrinking", () -> new PotionRingItem(new Item.Properties().stacksTo(1), PRTypes.none));
+			ring_of_widening = ITEMS_REGISTRY.register("ring_of_widening", () -> new PotionRingItem(new Item.Properties().stacksTo(1), PRTypes.none));
+			ring_of_thinning = ITEMS_REGISTRY.register("ring_of_thinning", () -> new PotionRingItem(new Item.Properties().stacksTo(1), PRTypes.none));
+		}
+	}
+}
